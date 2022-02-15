63b12711aeffe23c9180467e301015b5126b0c15
Tiago Nascimento <tiago.nascimento@spruceid.com>
2022-02-15 16:14:41 -0300

    Fix OmniAuth example resource manifest
    Signed-off-by: Tiago Nascimento <tiago.nascimento@spruceid.com>

--- omniauth/app/assets/config/manifest.js
+++ omniauth/app/assets/config/manifest.js
@@ -2,3 +2,11 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
+
+//= link siwe_rails/application.css
+//= link siwe_rails/background.css
+//= link siwe_rails/coinbase.svg
+//= link siwe_rails/modal_icon.png
+//= link siwe_rails/swe-landing.svg
+//= link siwe_rails/web3bundle.js
+//= link siwe_rails/siwe.js
